f7dce1a8c8c03b2654350d06dc2dc27303eca1bc
luciano <luciano.costa@42mate.com>
2022-08-07 18:11:32 -0300

    Added Show.vue.

---
+++ resources/js/Pages/Notes/Index.vue
@@ -0,0 +1,70 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+defineProps({
+    notes: Array
+});
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notes module
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6 ">
+                    <div class="md:col-span-1 ">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg text-gray-900">
+                                Note list
+                            </h3>
+                            <p class="text-sm text-gray-600">
+                                Take the correct record and run any function (view, edit, delete).
+                            </p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <table>
+                                <tr v-for="note in notes" :key="note.id">
+                                    <td class="border px-4 py-2">
+                                        {{ note.excerpt}}
+                                    </td>
+                                    <td class="px-4 py-2 ">
+                                        <Link class="view p-1 rounded-md " :href="route('notes.show', { id: note.id })">
+                                            View
+                                        </Link>
+                                    </td>
+                                    <td class="edit  px-4 py-2 ">
+                                        <Link :href="route('notes.edit',note.id)">
+                                        Edit
+                                        </Link>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<style scoped>
+
+    .edit{
+        color: #1d5123;
+    }
+    .view {
+        color: #ab2f9b;
+    }
+
+    .edit:hover, .view:hover {
+        text-decoration: underline;
+    }
+</style>
---
+++ resources/js/Pages/Notes/Show.vue
@@ -0,0 +1,62 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/inertia-vue3';
+defineProps({
+    note: Object
+});
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notes module
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md:gap-6 ">
+                    <div class="md:col-span-1 ">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg text-gray-900">
+                                Note detail
+                            </h3>
+                            <p class="text-sm text-gray-600">
+                                {{ note.excerpt}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <p>
+                                {{ note.content }}
+                            </p>
+
+                            <hr class="mt-6 mb-6" />
+
+                            <Link  class="link p-2 rounded-md" :href="route('notes.index')">
+                                Back to list
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<style scoped>
+    .link {
+        background-color: #2f50ab;
+        color: #fff;
+        text-transform: uppercase;
+    }
+
+    .link:hover {
+        background-color: #4a73e2;
+    }
+</style>
+
+
